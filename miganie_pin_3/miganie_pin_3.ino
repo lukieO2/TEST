b4771ba66273e512eeb5e36a1d27c3dbf7ccea31
Łukasz K <62339607+lukieO2@users.noreply.github.com>
2022-11-06 11:06:09 +0100

    Add files via upload

---
+++ miganie_pin_3/miganie_pin_3.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(3, OUTPUT); //Konfiguracja pinu 8 jako wyjście
+  
+}
+ 
+void loop() {
+
+  digitalWrite(3, HIGH); //Włączenie diody
+  delay(1000); //Odczekanie 1 sekundy
+  digitalWrite(3, LOW); //Wyłączenie diody
+  delay(1000); //Odczekanie jednej sekundy
+}
