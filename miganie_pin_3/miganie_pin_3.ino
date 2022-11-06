556675a1e90a1e7597fee5adcf8d2f0b3b149565
Łukasz K <lukie@o2.pl>
2022-11-06 12:15:40 +0100

    zmiana di

--- miganie_pin_3/miganie_pin_3.ino
+++ miganie_pin_3/miganie_pin_3.ino
@@ -7,6 +7,6 @@ void loop() {
 
   digitalWrite(3, HIGH); //Włączenie diody
   delay(1000); //Odczekanie 1 sekundy
-  digitalWrite(3, LOW); //Wyłączenie diody
+  digitalWrite(3, LOW); //Wyłączenie diody66g6gh
   delay(1000); //Odczekanie jednej sekundy
 }
